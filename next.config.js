ad5946973f5102ad7fe73b4cc8ed71c6189d87d7
jeonsooyoung <peace2332@naver.com>
2025-05-03 23:10:27 +0900

    Revert "Merge branch 'feature/login-ui' of https://github.com/codeit-fe-14-project2-team5/Wikid into feature/boards"
    This reverts commit 7dd9940f48308fc09a8eb5d4e639c16ce132bbdd, reversing
    changes made to 1dd93f924c332bfb288122a24406507b63aa6178.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
@@ -11,14 +13,6 @@ const nextConfig = {
     styledComponents: true,
   },
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://wikied-api.vercel.app/:path*",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
